2b34a052aa496a403ef221d808bd1b8f83298e6b
jacky <jacksonalmeidada@gmail.com>
2019-09-30 09:50:22 +0200

    Update 1.js

---
+++ act2/1/1.js
@@ -0,0 +1,32 @@
+
+
+
+ let alreadyVisitedSet=new Set("google.com","firefox.com","opera.com")
+ extractedURLsArray=["google.com","origen.com","microsoft.com"] 
+
+
+
+function shouldBeVisited(alreadyVisitedSet, extractedURLsArray){
+
+
+if ()
+
+
+}
+
+
+
+
+// function shouldbeVisited(extractedURLsArray,alreadyVisitedSet){
+//     let nuevoConjunto=new set();
+//     for (let i=0; i<extractedURLsArray.length; i++) {
+//         for (let x = 0; x < alreadyVisitedSet.length; x++) {
+//             if (extractedURLsArray[i] == alreadyVisitedSet[x]) {
+                
+//             }else{
+//                 nuevoConjunto.add(extractedURLsArray[i])
+//             }
+//         }
+//     }
+
+// }
